67e2d85ef3f879176c374ee788541b2761011001
Mohamad Moheb <mohamadmoheb7@gmail.com>
2021-12-27 07:32:20 +0200

    Update main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        System.out.print("What is the currency of your money")
+	}
+}
